5fe5acd5fe707bc982b8d85093902d935f920dfb
Paul Kordes <paul.kordes@contino.io>
2023-05-17 15:06:38 -0400

    still working on 03

---
+++ 02-webserver/scripts/help.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Targets:"
+echo "    run: launches the app"
+echo "destroy: destroys the app"
+echo "   test: runs the application tests"
+echo "    all: executes run, test, destroy in that order"
+echo "   help: prints this message"
