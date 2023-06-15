89eab46924c2e7ff17e3899c613e31704e4a281a
gpatmore <greg.patmore@contino.io>
2023-06-15 10:30:48 -0400

    fixed the test to match the message change

--- 02-webserver/scripts/run-tests.sh
+++ 02-webserver/scripts/run-tests.sh
@@ -7,4 +7,4 @@ echo -n "Checking Web server is up..."
 curl -ILs ${WEB_SERVER_ADDRESS} | grep ^HTTP | grep -q 200 && echo PASS || echo FAIL
 
 echo -n "Checking Expected Page Content..."
-curl -s ${WEB_SERVER_ADDRESS} | grep -q "Hello, Cloud Native Computing Baltimore!" && echo PASS || echo FAIL
+curl -s ${WEB_SERVER_ADDRESS} | grep -q "Hello, Geekfest!" && echo PASS || echo FAIL
